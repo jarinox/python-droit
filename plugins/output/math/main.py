03e55c0de994b93dfd45feccf635c9dde8b27e31
jaybeejs <45308098+jaybeejs@users.noreply.github.com>
2019-05-18 17:48:31 +0200

    first commit

---
+++ plugins/output/math/main.py
@@ -0,0 +1,13 @@
+import math
+
+def equal(inp):
+	inp = inp.replace("mal", "*").replace("durch", "/").replace("minus", "-").replace("plus", "+").replace("hoch", "**").replace("pi", str(math.pi)).replace("sqrt", "math.sqrt").replace("wurzel", "math.sqrt")
+	inp = inp.replace("^", "**").replace(":", "/").replace("²", "**2").replace("³", "**3").replace(" ", "")
+	try:
+		return str(eval(inp))
+	except:
+		print("Die Berechnung konnte nicht abgeschlossen werden")
+
+
+
+info = "Dieses Plugins kann einfache Berechnungen durchführen"
