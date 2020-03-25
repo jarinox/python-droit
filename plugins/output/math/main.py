43c09556304ef501d34dc36a5457053d52d02f44
jaybeejs <45308098+jaybeejs@users.noreply.github.com>
2020-03-25 11:07:57 +0100

    license notices added

--- plugins/output/math/main.py
+++ plugins/output/math/main.py
@@ -1,3 +1,23 @@
+# main.py - math plugin for python-droit
+# Copyright 2019 Jakob Stolze
+#
+#
+# This file is part of python-droit.
+#
+# python-droit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# python-droit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with python-droit.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import math
 
 def equal(inp, rpack):
