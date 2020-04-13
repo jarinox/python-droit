9570529c601ce08a4e1519887f4f73de141b07e0
jaybeejs <45308098+jaybeejs@users.noreply.github.com>
2020-04-13 15:50:01 +0200

    sample output plugin added

---
+++ droit/plugins/output/math/main.py
@@ -0,0 +1,5 @@
+def calc(data, rpack):
+	equation = data[0].lower().replace("plus", "+").replace("minus", "-")
+	equation = equation.replace("^", "**").replace("x", "*").replace(",", ".")
+	return eval(equation)
+
